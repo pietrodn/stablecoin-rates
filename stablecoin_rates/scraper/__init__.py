2e30c3f98383666aff8b18f1d8f132cfbb42d6ad
pietrodn <powerpdn@gmail.com>
2021-12-26 11:26:45 +0100

    feat: add Anchor Protocol

--- stablecoin_rates/scraper/__init__.py
+++ stablecoin_rates/scraper/__init__.py
@@ -1,6 +1,7 @@
 from typing import Callable, Awaitable
 
 from stablecoin_rates.model import LendingRate
+from stablecoin_rates.scraper.anchor import get_anchor_rates
 from stablecoin_rates.scraper.ftx import get_ftx_rates
 from stablecoin_rates.scraper.huobi import (
     get_huobi_flexible_rates,
@@ -15,6 +16,7 @@ SCRAPER_METHODS: list[Callable[[], Awaitable[list[LendingRate]]]] = [
     get_huobi_flexible_rates,
     get_huobi_fixed_rates,
     get_huobi_featured_rates,
+    get_anchor_rates,
 ]
 
 __all__ = [
@@ -24,4 +26,5 @@ __all__ = [
     "get_huobi_flexible_rates",
     "get_huobi_fixed_rates",
     "get_huobi_featured_rates",
+    "get_anchor_rates",
 ]
---
+++ stablecoin_rates/scraper/anchor.py
@@ -0,0 +1,33 @@
+from decimal import Decimal
+from typing import Any
+
+from stablecoin_rates.model import LendingRate
+import httpx
+
+
+# See https://github.com/Anchor-Protocol/anchor-earn/blob/master/src/facade/terra-anchor-earn.ts
+BLOCKS_IN_YEAR = 4_656_810
+ANCHOR_RATE_URL = "https://api.anchorprotocol.com/api/v2/deposit-rate"
+
+
+async def get_anchor_rates() -> list[LendingRate]:
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(ANCHOR_RATE_URL)
+        json_response = response.json(parse_float=Decimal, parse_int=Decimal)
+
+    return parse_anchor_rate(json_response)
+
+
+def parse_anchor_rate(response: list[dict[str, Any]]) -> list[LendingRate]:
+    platform = "Anchor"
+    rate = response[0]
+    return [
+        LendingRate(
+            asset="UST",
+            platform=platform,
+            apr=Decimal(rate["deposit_rate"]) * BLOCKS_IN_YEAR,
+            duration=None,
+            project_name="Earn",
+        )
+    ]
